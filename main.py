b6fe4f59af8c5871be152e6b3052a8a312a6a1c3
Gyuan1 <2843311525@qq.com>
2025-09-17 10:00:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 weight = float(input("请输入重量数值："))
 unit = input("请输入单位（kg 表示千克，lb 表示磅）：")
 if unit == "kg":
-     pound = weight * 2.20462
-     print(f"{weight} 千克 = {pound:.2f} 磅")
+    pound = weight * 2.20462
+    print(f"{weight} 千克 = {pound:.2f} 磅")
 elif unit == "lb":
-     kg = weight / 2.20462
-     print(f"{weight} 磅 = {kg:.2f} 千克")
+    kg = weight / 2.20462
+    print(f"{weight} 磅 = {kg:.2f} 千克")
 else:
-     print("请输入有效的单位（kg 或 lb）")
+    print("请输入有效的单位（kg 或 lb）")
